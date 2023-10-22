4b73ad996c78d3962d69559f825388d1ba81baff
mirza-azam <mirzaazam884@gmail.com>
2023-10-22 08:56:35 +0600

    problem fixed

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ document.getElementById('login-btn').addEventListener('click', function(){
     const inputEmailText = document.getElementById('email-input').value;
     const inputPasswordText = document.getElementById('password-input').value;
     if( inputEmailText == 'mirzaazam@gmail.com' && inputPasswordText == 'nohi12345' ){
-        location.href = 'index.html';
+        location.href = "html/index.html";
     }
     else{
         alert('Invalid user or password!');
